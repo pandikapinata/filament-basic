42cc3490f1bb29b6282974400fcd175b02a1c517
pandikapinata <pandikapinata27@gmail.com>
2024-06-29 22:32:34 +0800

    Customize Dashboard - Add some colors, change the font, add logo, and favicon - Adjust the menu, change the icons

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,8 +28,18 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'danger' => Color::Rose,
+                'gray' => Color::Slate,
+                'info' => Color::Blue,
+                'primary' => Color::Indigo,
+                'success' => Color::Emerald,
+                'warning' => Color::Orange,
             ])
+            ->font('Poppins')
+            ->brandName('Filament Basic')
+            ->brandLogo(fn () => view('filament.app.logo'))
+            ->brandLogoHeight('1.25rem')
+            ->favicon(asset('images/favicon.ico'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
